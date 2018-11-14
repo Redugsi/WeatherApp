020d5400ec5e7a5b3dd4313fe0fc57b46a91d260
MRedugsi <erdemisg@gmail.com>
2018-11-14 19:39:01 +0300

    Fix

--- app/src/main/java/com/redugsi/weatherapp/api/ApiInterceptor.kt
+++ app/src/main/java/com/redugsi/weatherapp/api/ApiInterceptor.kt
@@ -9,7 +9,7 @@ class ApiInterceptor : Interceptor {
         val url: HttpUrl = chain.request().url().newBuilder().addQueryParameter("APPID", "2115a4940bd995832856dc56e1673bbd").
                 addQueryParameter("mode", "json").
                 addQueryParameter("units", "metric").build()
-        return chain.proceed(chain.request().newBuilder().addHeader("Accept", "application/json").url(url).build())
+        return chain.proceed(chain.request().newBuilder().addHeader("Content-Type", "application/json; charset=utf-8").url(url).build())
     }
 
 }
